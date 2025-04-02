42073f69889e4ff9aa68998c8285fbc0aadb06c9
Abhiram98 <bellur.abhiram@gmail.com>
2025-04-01 19:29:31 -0600

    fix bug in test

--- tests/test_grazie.py
+++ tests/test_grazie.py
@@ -5,7 +5,7 @@ from grazie.api.client.endpoints import GrazieApiGatewayUrls
 from grazie.api.client.gateway import AuthType
 from grazie_langchain_utils.language_models.grazie import ChatGrazie
 from grazie_langchain_utils.callbacks import GrazieInfoCallbackHandler
-from pydantic import SecretStr
+from pydantic.v1 import SecretStr
 from langchain_core.messages import SystemMessage, AIMessage, ToolMessage, HumanMessage
 
 def test_grazie():
