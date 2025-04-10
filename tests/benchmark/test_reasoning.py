2399ccf9b67c973f3e354524776fa9fc3b4ec943
Abhiram98 <bellur.abhiram@gmail.com>
2025-04-10 12:31:45 -0600

    type cast to str

--- tests/benchmark/test_reasoning.py
+++ tests/benchmark/test_reasoning.py
@@ -25,7 +25,7 @@ def test_reasoning():
         model=grazie_llm,
         initial_intent="please split up methods into reusable code fragments",
         source_code=project.get_file_contents(rel_file_path),
-        source_file_path=rel_file_path
+        source_file_path=str(rel_file_path)
     ).compile()
 
     result = compiled_flow.invoke({
@@ -50,7 +50,7 @@ def test_reasoning2():
         model=grazie_llm,
         initial_intent="please split up methods into reusable code fragments",
         source_code=project.get_file_contents(rel_file_path),
-        source_file_path=rel_file_path
+        source_file_path=str(rel_file_path)
     ).compile()
 
     result = compiled_flow.invoke({
@@ -76,7 +76,7 @@ def test_reasoning_flink_2():
         model=grazie_llm,
         initial_intent="Introduce the interface and default implementation of TaskInfo",
         source_code=project.get_file_contents(rel_file_path),
-        source_file_path=rel_file_path
+        source_file_path=str(rel_file_path)
     ).run()
 
     print(refactoring_plan)
