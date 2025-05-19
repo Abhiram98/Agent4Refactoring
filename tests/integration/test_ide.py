8edf9277f86a8e9b7eb3f160c5cee071333f93ed
Abhiram98 <bellur.abhiram@gmail.com>
2025-05-18 18:09:23 -0600

    fix test

--- tests/integration/test_ide.py
+++ tests/integration/test_ide.py
@@ -8,12 +8,13 @@ def test_empty_inspections():
     intellij_server = ij.IntellijServer(server_url=refagent.IJ_SERVER_URL)
     project.checkout('9f64053dc0bb21a2d8714f4fca5ae58cbaef2a7d', force=True)
     intellij_server.open_project(project_path=project.get_project_path())
+    intellij_server.reset_project_reload_counters()
     intellij_server.reload_project()
     intellij_server.open_file(rel_file_path=Path("ratpack-core/src/main/java/ratpack/override/Override.java"))
 
 
     inspections = intellij_server.run_code_inspection(1)
-    assert inspections == []
+    assert inspections == '[]'
 
 def test_rename():
     project = pm.EvalProject("ratpack")
