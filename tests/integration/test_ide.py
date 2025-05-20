29363626e9537ff1b9ebce9f3db6d1963fb94b66
Abhiram98 <bellur.abhiram@gmail.com>
2025-05-19 21:23:30 -0600

    add tests for ide server

--- tests/integration/test_ide.py
+++ tests/integration/test_ide.py
@@ -26,3 +26,58 @@ def test_rename():
 
     result = intellij_server.call_tool('rename', old_name='UserRegistryOverrides', new_name='UserRegistryImpositions')
     assert result=='success'
+
+
+def test_rename_overridden_method():
+    project = pm.EvalProject("ratpack")
+    intellij_server = ij.IntellijServer(server_url=refagent.IJ_SERVER_URL)
+    project.checkout('d803134afbaa200920b95007ab6cb4e975fc2b5c', force=True)
+    intellij_server.open_project(project_path=project.get_project_path())
+
+    intellij_server.reset_project_reload_counters()
+    intellij_server.reload_project()
+    intellij_server.open_file(
+        rel_file_path=Path("ratpack-groovy/src/main/java/ratpack/groovy/"
+                           "internal/RatpackDslClosureToHandlerTransformer.java"))
+
+    result = intellij_server.call_tool('rename', old_name='modules', new_name='bindings')
+    print(result)
+
+
+
+def test_rename_overridden_method_flink():
+    project = pm.EvalProject("flink")
+    intellij_server = ij.IntellijServer(server_url=refagent.IJ_SERVER_URL)
+    project.checkout('21403e31f4761bdddf5e4e802e0e5eb9b4533202', force=True)
+    intellij_server.open_project(project_path=project.get_project_path())
+
+    intellij_server.reset_project_reload_counters()
+    intellij_server.reload_project()
+    intellij_server.open_file(
+        rel_file_path=Path("flink-runtime/src/main/java/org/apache/flink/runtime/state/filesystem/FsStateBackend.java"))
+
+
+    result = intellij_server.call_tool('rename',
+                                       old_name='createOperatorStateBackend',
+                                       new_name='createOperatorStateBackend2')
+    print(result)
+
+
+
+def test_rename_class_flink():
+    project = pm.EvalProject("flink")
+    intellij_server = ij.IntellijServer(server_url=refagent.IJ_SERVER_URL)
+    project.checkout('21403e31f4761bdddf5e4e802e0e5eb9b4533202', force=True)
+    intellij_server.open_project(project_path=project.get_project_path())
+
+    intellij_server.reset_project_reload_counters()
+    intellij_server.reload_project()
+    intellij_server.open_file(
+        rel_file_path=Path("flink-runtime/src/main/java/org/apache/flink/runtime/state/filesystem/FsStateBackend.java"))
+
+
+    result = intellij_server.call_tool('rename',
+                                       old_name='FsStateBackend',
+                                       new_name='FsStateBackend2')
+    print(result)
+
