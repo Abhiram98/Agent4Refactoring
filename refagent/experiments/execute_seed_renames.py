0f98f86ad58200112a4f61c92c5182b4208d9935
Abhiram98 <bellur.abhiram@gmail.com>
2025-06-03 16:52:46 -0600

    check in a few seed renames

--- refagent/experiments/execute_seed_renames.py
+++ refagent/experiments/execute_seed_renames.py
@@ -11,9 +11,9 @@ import refagent.refactoring_types.refactorings as refactorings
 
 
 def main():
-    print("creating seed renames for argouml")
+    print("creating seed renames for renas dataset")
 
-    with open(refagent.data_folder.joinpath('renas/argouml.json')) as f:
+    with open(refagent.data_folder.joinpath('renas/renas_oracle.json')) as f:
         data = json.load(f)
     rename_data = benchmark_load.load_benchmark(data, bench_type=benchmark_load.RenameItem)
     ij_server = ij.IntellijServer(server_url=refagent.IJ_SERVER_URL)
@@ -25,6 +25,7 @@ def main():
     co_renames = [i for i in groups if len(i[1][i[1]['conceptRename?'] == 'TRUE']) >= 1]
 
     for r in rename_data:
+        print(f"Creating seed example for {r.ref_id}")
         if r.seed_hash is not None:
             print(f"Seed computation was completed for {r.ref_id}")
             continue
@@ -40,9 +41,10 @@ def main():
 
         co_rename_df = \
         [i[1] for i in co_renames if i[0][0] == r.v2_hash and i[0][1] == r.corename_id][0]
-        concept = sorted(co_rename_df[['oldName', 'newName', 'type', 'file', 'line']].to_dict(orient='records'),
+        all_concepts = sorted(co_rename_df[['oldName', 'newName', 'type', 'file', 'line']].to_dict(orient='records'),
                          key=scrape_rename.name_sort_key)
-        concept = concept[0]
+        concept = all_concepts[0]
+        # for concept in all_concepts:
         # seed_rename = scrape_rename.RenameRecommendation.from_renas_oracle(concept)
         possible_examples = [i for i in r.refactoring_changes if isinstance(i, refactorings.Rename)
                           and i.old_name == concept['oldName']
@@ -51,7 +53,10 @@ def main():
                           and concept['type'].lower() in i.type.lower()
                           and i.leftSideLocations[0].filePath == concept['file']
                           ]
-        assert len(possible_examples) == 1
+            # if len(possible_examples) == 1:
+            #     break
+        if len(possible_examples) != 1:
+            continue
         r.seed_example = possible_examples[0]
 
 
@@ -60,14 +65,19 @@ def main():
                        "line_num": concept['line'],
                        "code_element_type": concept['type'].lower()}
         tool_call_status = ij_server.call_tool('rename', **seed_rename_json)
+        if tool_call_status != 'success':
+            # tool_call_status = ij_server.call_tool('rename', old_name=concept['oldName'],
+            #                                        new_name=concept['newName'])
+            print("too call failed.")
+            continue
         assert tool_call_status == 'success'
         changed_files = project.get_changed_files()
         project.safe_add(changed_files)
         new_hash = project.git_repo.index.commit(f"seed rename for {r.ref_id}")
-        seed_hashes[r.ref_id] = new_hash
-        r.seed_hash = new_hash
+        seed_hashes[r.ref_id] = str(new_hash)
+        r.seed_hash = str(new_hash)
 
-        with open(refagent.data_folder.joinpath('renas/argouml.json'), 'w') as f:
+        with open(refagent.data_folder.joinpath('renas/renas_oracle.json'), 'w') as f:
             final_data = [i.to_json() for i in rename_data]
             json.dump(final_data, f, indent=4)
 if __name__ == '__main__':
