519e08a09270a79a1cd6636a84d1670376b07b33
Abhiram98 <bellur.abhiram@gmail.com>
2025-09-17 12:02:51 -0600

    script to summarize renames

---
+++ refagent/benchmark/rename_summary.py
@@ -0,0 +1,30 @@
+import argparse
+import json
+import sys
+
+import refagent.benchmark.load as bm_load
+from collections import defaultdict
+
+if __name__ == "__main__":
+    file_path = str(sys.argv[1])
+    id = int(sys.argv[2])
+    with open(file_path, 'r') as f:
+        json_ = json.load(f)
+    rename_bench = bm_load.load_benchmark(json_, bench_type=bm_load.RenameItem)
+
+
+    file_groups = defaultdict(list)
+    for i in rename_bench:
+        if i.ref_id == id:
+            for i in i.refactoring_changes:
+                file_groups[i.leftSideLocations[0].filePath].append(i)
+
+    for file_name in file_groups:
+        print(file_name)
+        for rename in file_groups[file_name]:
+            print(rename.description)
+        print("----")
+
+    print("all files renamed:")
+    for file_name in file_groups:
+        print(f"file: {file_name}")
