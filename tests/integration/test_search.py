aa616b68bc736bdc7102296989f71f1d4514558f
Abhiram98 <bellur.abhiram@gmail.com>
2025-09-18 17:07:52 -0600

    add test search for restoreMode

--- tests/integration/test_search.py
+++ tests/integration/test_search.py
@@ -24,3 +24,22 @@ def test_flink_test_method2():
     assert 'hit_count' in response_dict['files'][0]
     assert 'line_nums' in response_dict['files'][0]
     assert len(response_dict['files']) > 2
+
+def test_flink_test_restore_mode():
+    project = pm.EvalProject("flink")
+    project.checkout("be25a140f011e6ff93a23f28b3826d376a1c0ba7")
+    intellij_server = ij.IntellijServer(server_url=refagent.IJ_SERVER_URL)
+    intellij_server.open_project(project_path=project.get_project_path())
+    intellij_server.open_file(
+        rel_file_path=Path('flink-runtime-web/src/main/java/org/apache/flink/runtime/webmonitor/handlers/JarRunRequestBody.java'))
+    intellij_server.reset_project_reload_counters()
+    intellij_server.reload_project()
+    response = intellij_server.call_tool('search_symbol',
+                                         symbol='restoreMode')
+    response_dict = json.loads(response)
+    assert 'hit_count' in response_dict
+    assert 'files' in response_dict
+    assert 'file_path' in response_dict['files'][0]
+    assert 'hit_count' in response_dict['files'][0]
+    assert 'line_nums' in response_dict['files'][0]
+    assert len(response_dict['files']) == 2
